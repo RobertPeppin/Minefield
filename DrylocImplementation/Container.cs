aa5054f9b01e7fb941eb2815e1bb2ab3cd18a61b
Robert P <34028964+RobertPeppin@users.noreply.github.com>
2021-10-20 21:01:33 +0100

    Move my change to DryIoC to separate file

---
+++ DrylocImplementation/Container.cs
@@ -0,0 +1,23 @@
+﻿namespace DryIoc
+{
+    public partial class Container
+    {
+        private static Container instance;
+        public static Container Current
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Container(rules => rules.WithDefaultReuse(Reuse.Singleton));
+                }
+                else if (instance.IsDisposed)
+                {
+                    instance = new Container(rules => rules.WithDefaultReuse(Reuse.Singleton));
+                }
+
+                return instance;
+            }
+        }
+    }
+}
